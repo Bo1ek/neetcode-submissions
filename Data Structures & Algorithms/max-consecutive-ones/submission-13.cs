bc340a61acd487630ebe4883467aad38d8e01529
Jakub Bolek <98595825+Bo1ek@users.noreply.github.com>
2026-07-30 22:34:44 +0200

    Add: max-consecutive-ones - submission-13

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-13.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        foreach (int num in nums)
+        {
+            currentCount = (num == 1) ? currentCount + 1 : 0;
+            maxCount = Math.Max(currentCount,maxCount);
+        }
+        return maxCount;
+    }
+}
